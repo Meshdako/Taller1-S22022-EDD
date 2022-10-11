960471df9f2cba325cf21b26fbdfeb48af9e2a4c
Meshdako <msobarzo@utem.cl>
2022-10-11 00:16:05 -0300

    Menu2 y Menu3, agregadas variables de entrada a las funciones.

--- funciones.h
+++ funciones.h
@@ -24,12 +24,12 @@ void CantidadDeElectores(Persona [], int);
 objetos, la cantidad de elementos del vector y una variable string que utilizará
 para comparar por comunas y mostrar los datos ordenados de forma
 ascendente. */
-void ElectoresPorComuna(Persona [], int);
+void ElectoresPorComuna(Persona Electores[], int indice, char Comuna[256]);
 
 /* Implementa una función cuyos parámetros de entrada son un vector de
 objetos, cantidad de elementos del vector y un valor que representará un rut
 a comprar para entregar por pantalla todos los datos del elector. */
-void ConsultaDeDatos(Persona [], int);
+void ConsultaDeDatos(Persona Electores[], int indice, int BuscarRun);
 
 /* Implementa una función cuyos parámetros de entrada son un vector de
 objetos y la cantidad de elementos del vector y realiza el procesamiento para
